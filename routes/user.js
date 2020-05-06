eaba6bb5e3531bd9db4e9341b2cc44a90aa95662
Katherine Limanu <katherinelimanu21@gmail.com>
2020-05-06 13:17:58 +0700

    pakai findAll findOne

--- routes/user.js
+++ routes/user.js
@@ -3,19 +3,23 @@ const router = express.Router();
 const sequelize = require('../database');
 const jwt = require("jsonwebtoken");
 
+const userModel = require("../models/user");
+const User = userModel;
+
 const secretkey = process.env.SECRET_KEY;
 
-router.get("/", (req, res)=>{
-    res.send("ini user");
+router.get("/", async (req, res)=>{
+    let resUser = await User.findAll();
+    res.json(resUser);
+    // res.send("ini user");
 });
 
 getUser= async(email_user, password_user=null)=>{
-    let qry= `select * from users where email_user = '${email_user}'`;
-    if(password_user)qry += ` and password_user ='${password_user}'`;
+    let resUser = await User.findOne({
+        where: {"email_user": email_user}
+    })
 
-    let resUser = await sequelize.query(qry);
-    console.log(resUser);
-    if(resUser[0].length==0){
+    if(!resUser){
         return false
     }else{
         return true
@@ -30,11 +34,15 @@ router.post("/register", async (req, res)=>{
     if(!email_user || !nama_user || !password_user) res.status(400).send("Data harus lengkap");
     else{
         let found = await getUser(email_user);
-        console.log("found", found);
         if(found) res.status(200).send("Email sudah dipakai");
         else{
             try{
-                let newUser = await sequelize.query(`insert into users (email_user, password_user, nama_user) values ('${email_user}','${password_user}','${nama_user}')`);
+                let newUser = User.build({
+                    email_user: email_user,
+                    password_user: password_user,
+                    nama_user: nama_user
+                })
+                newUser.save();
                 res.status(200).send("Insert user baru berhasil");
             }catch(error){
                 res.status(400).send(error);
@@ -60,11 +68,7 @@ router.post('/login', async(req, res)=>{
             res.status(400).send("User tidak ditemukan");
         }
     }
-    
 })
-
-
-
 
 
 module.exports=router;
