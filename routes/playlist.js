acc0b03410448f31e14d72394761e0885ab7f497
Matt <darknesswish7@gmail.com>
2020-05-25 17:21:38 +0700

    belom di test

--- routes/playlist.js
+++ routes/playlist.js
@@ -178,18 +178,18 @@ router.put('/update',async(req,res)=>{
             let user = jwt.verify(token, process.env.SECRET_KEY);
             let id_playlist=req.body.id_playlist,
             deskripsi_playlist = req.body.deskripsi_playlist,jenis_playlist=req.body.jenis_playlist;
-            if(!id_playlist) res.status(400).send("ID Playlist wajib dicantumkan");
-            else if(!jenis_playlist)res.status(400).send("Tipe Playlist Harap Dicantumkan");
+            if(!id_playlist) res.status(400).send({message:"ID Playlist wajib dicantumkan"});
+            else if(!jenis_playlist)res.status(400).send({message:"Tipe Playlist Harap Dicantumkan"});
             else{
                 let dataplaylist = await Playlist.findOne({
                     where: {"email_user": user.email_user,"id_playlist":id_playlist}
                 })
-                if(dataplaylist==null)res.status(400).send("Playlist Tidak Ditemukan");
+                if(dataplaylist==null)res.status(400).send({message:"Playlist Tidak Ditemukan"});
                 else{
                     dataplaylist.deskripsi_playlist=deskripsi_playlist;
                     dataplaylist.jenis_playlist=jenis_playlist;
                     await dataplaylist.save();
-                    res.status(200).send("Sukses Update Playlist");
+                    res.status(200).send({message:"Sukses Update Playlist"});
                 }
              
             }
@@ -199,7 +199,32 @@ router.put('/update',async(req,res)=>{
     }
 });
 
-
+router.delete('/delete',async(req,res)=>{
+    const token = req.headers['x-auth-token'];
+    console.log(token);
+    if(!token) res.status(404).send("Token not found!");
+    else{
+        try{
+            let user = jwt.verify(token, process.env.SECRET_KEY);
+            let id_playlist=req.body.id_playlist,
+            id_lagu=req.body.id_lagu;
+            if(!id_playlist) res.status(400).send({message:"ID Playlist wajib dicantumkan"});
+            else{
+                let dataplaylist = await Playlist.findOne({
+                    where: {"email_user": user.email_user,"id_playlist":id_playlist}
+                })
+                if(dataplaylist==null)res.status(400).send({message:"Playlist Tidak Ditemukan"});
+                else{
+                    await Detail.destroy({where:{"id_track":id_lagu}});
+                    res.status(200).send({message:"Lagu dengan id "+id_lagu+" berhasil dihapus"});
+                }
+             
+            }
+        }catch(err){
+            res.status(400).send(err);
+        }
+    }
+});
 
 const checkuser = async(token, callback)=>{
     const SECRET = process.env.SECRET_KEY;
